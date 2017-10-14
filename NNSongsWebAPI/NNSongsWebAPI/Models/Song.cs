f238635ad7a112712dc624cd47e7b9291073f4c6
Navjot-Nagi <31776059+Navjot-Nagi@users.noreply.github.com>
2017-10-14 11:18:43 -0400

    Start
    Start of project in git hub

---
+++ NNSongsWebAPI/NNSongsWebAPI/Models/Song.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NNSongsWebAPI.Models
+{
+    public class Song
+    {
+        private int songID;
+        private string title;
+        private string albumName;
+        private string artist;
+        private string genre;
+
+        public Song()
+        { }
+
+        public Song(int songID, string title, string albumName, string artist, string genre)
+        {
+            this.songID = songID;
+            this.title = title;
+            this.albumName = albumName;
+            this.artist = artist;
+            this.genre = genre;
+
+        }
+
+        public int SongID
+        {
+            get { return songID; }
+            set { songID = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string AlbumName
+        {
+            get { return albumName; }
+            set { albumName = value; }
+        }
+
+        public string Artist
+        {
+            get { return artist; }
+            set { artist = value; }
+        }
+
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+
+    }
+}
---
+++ NNSongsWebAPI/NNSongsWebAPI/Models/SongList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NNSongsWebAPI.Models
+{
+    public class SongList
+    {
+        private static List<Song> songs = new List<Song>()
+        {
+            new Song { SongID = 1, Title = "Despacito", AlbumName = "Despacito", Artist = "Luis Fonsi", Genre = "Reggaeton"},
+            new Song { SongID = 2, Title = "Do you know", AlbumName = "Do you know", Artist = "Diljit Dosanjh", Genre = "Punjabi Bhangra romentic pop sikh"},
+            new Song { SongID = 3, Title = "Aise na mujhe tum dekho", AlbumName = "DJ Suketu & Friends Present RD Burman Reinvented", Artist = "DJ Suketu", Genre = "Pop"},
+            new Song { SongID = 4, Title = "Baby I like your style ", AlbumName = "Views", Artist = "Drake", Genre = "Afrobeat, Hip-hop/rap"},
+
+        };
+
+        public static List<Song> GetAll()
+        {
+            return songs;
+        }
+
+        public static Song GetSong(int songID)
+        {
+            return songs.Find(x => x.SongID == songID);
+        }
+
+        public static void CreateSong(Song newSong)
+        {
+            songs.Add(newSong);
+           
+        }
+
+        public static void updateSong(int songID, Song newSong)
+        {
+            songs.Remove(songs.Find(x => x.SongID == songID));
+            songs.Add(newSong);
+        }
+
+        public static void DeleteSong(int songID)
+        {
+            songs.Remove(songs.Find(x => x.SongID == songID));
+        }
+    }
+}
